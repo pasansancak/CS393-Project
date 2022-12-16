47b7c3900c0ea463b2443a299f54f38798b29c7e
pasan.sancak <pasan.sancak@ozu.edu.tr>
2022-12-16 11:29:10 +0300

    first commit

---
+++ src/main/java/com/example/CS393_Project1/Cs393Project1Application.java
@@ -0,0 +1,13 @@
+package com.example.CS393_Project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs393Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs393Project1Application.class, args);
+	}
+
+}
